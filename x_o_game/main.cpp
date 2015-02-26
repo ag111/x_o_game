94e017e89957708293beacfaa61b222372b8711a
Agness <agness@MacBook-Pro-Agness.local>
2015-02-26 13:29:10 +0300

    Initial Commit

---
+++ x_o_game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  x_o_game
+//
+//  Created by Agness on 26.02.15.
+//  Copyright (c) 2015 Agness Ansh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
